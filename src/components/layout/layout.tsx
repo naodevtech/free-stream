8c6883a51e52331252d1d48afedfde30d1de4b7d
naodevtech <naimtahouri@gmail.com>
2022-02-05 14:58:07 +0100

    :fire: add environment project with architecture basics :fire:

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import Nav from "../organisms/navigation/navigation";
+
+type Props = {
+  children: JSX.Element;
+};
+
+const Layout = ({ children }: Props) => {
+  return (
+    <>
+      <Nav />
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
